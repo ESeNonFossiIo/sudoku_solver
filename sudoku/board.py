0666b0d344413b643590af477c4f4cf8960469ba
ESeNonFossiIo <esenonfossiio@gmail.com>
2025-01-01 15:29:08 +0000

    Add more tests

--- sudoku/board.py
+++ sudoku/board.py
@@ -2,24 +2,26 @@ import numpy as np
 from collections import Counter
 
 
-def __assert_no_duplicates(values):
-    """ Assert that there are no duplicates in the list of values.
-
+def _check_no_duplicates(values):
+    """ Check if there are no duplicates in the list of values.
         Args:
-            values (list): List of values to be checked.
+            values (list): List of values.
+        Returns:
+            bool: True if there are no duplicates, False.
+                Zero values are ignored.
     """
-    counter = Counter(values)
-    for value, count in counter.items():
-        assert (count <= 1) or (value == 0), "Duplicate non-0 value %d" % value
+    return all([(count <= 1) or (value == 0)
+                for value, count in Counter(values).items()])
 
 
-def __assert_valid_value(value):
-    """ Assert that the value is valid.
-
+def _check_valid_value(value):
+    """ Check if the value is valid, i.e., if it is between 0 and 9.
         Args:
             value (int): Value to be checked.
+        Returns:
+            bool: True if the value is valid, False otherwise.
     """
-    assert 0 <= value <= 9, "Invalid value %d" % value
+    return 0 <= value <= 9
 
 
 class Board(object):
@@ -40,6 +42,23 @@ class Board(object):
         # valid.
         self.__values = np.ones((9, 9, 9), dtype=bool)
 
+    def __str__(self):
+        """ Return a string representation of the board.
+            Returns:
+                str: String representation of the board.
+        """
+        s = ""
+        for i in range(9):
+            if i % 3 == 0:
+                s += "+---------+---------+---------+" + "\n"
+            for j in range(9):
+                if j % 3 == 0:
+                    s += "|"
+                s += " {} ".format(self.__board[i, j])
+            s += "|" + "\n"
+        s += "+---------+---------+---------+" + "\n"
+        return s
+
     def __setitem__(self, key, value):
         """ Set the value of a cell in the board.
 
@@ -47,30 +66,68 @@ class Board(object):
                 key (int, int): Tuple with the coordinates of the cell.
                 value (int): Value to be set in the cell.
         """
-        __assert_valid_value(value)
+        assert _check_valid_value(value)
         self.__board[key] = value
+        # TODO: can we avoid updating all the values? just work on key?
+        self.__update_values() 
+
+    def __getitem__(self, key):
+        """ Get the value of a cell in the board.
+
+            Args:
+                key (int, int): Tuple with the coordinates of the cell.
+
+            Returns:
+                int: Value of the cell.
+        """
+        return self.__board[key]
 
     def isValid(self):
-        """ Check if the board is valid.
-
+        """ Check if the board is valid. i.e., if there are no duplicates in rows, columns and sub-grids.
+        
             Returns:
                 bool: True if the board is valid, False otherwise.
         """
-        # Check if there are no duplicates in the rows and in the columns.
-        for i in range(9):
-            __assert_no_duplicates(self.__board[i, :])
-            __assert_no_duplicates(self.__board[:, i])
-        # Check if there are no duplicates in the 3x3 sub-grids.
-        for i in range(3):
-            for j in range(3):
-                __assert_no_duplicates(
-                    self.__board[3 * i:3 * i + 3, 3 * j:3 * j + 3].flatten())
+        # Check if there are no duplicates in the rows, columns and sub-grids.
+        if not all([_check_no_duplicates(self.__board[i, :]) for i in range(9)]):
+            return False
+        elif not all([_check_no_duplicates(self.__board[:, i]) for i in range(9)]):
+            return False
+        elif not all([_check_no_duplicates(self.__board[3 * i:3 * i + 3, 3 * j:3 * j + 3].flatten())
+                      for i in range(3) for j in range(3)]):
+            return False
+        else:
+            return True
 
     def __update_values(self):
-        """ Update the values of the board. """
+        """ Update the values of __values using the values of __board.
+            If the value of a cell of __board is not zero,
+            the corresponding values in __values are set to True
+            and all the other values are set to False.
+        """
+        # TODO:
+
+    def set_matrix(self, matrix: list[list[int]]):
+        """ Set the values of the board using a matrix.
+
+            Args:
+                matrix (list of lists): List of lists with the values of the board.
+        """
+        # TODO: assert matrix.shape == (9, 9)
+        self.__board = np.array(matrix)
+        self.__update_values()
+
+    def discover_values(self):
+        """ Discover the values of the board using the values of __values.
+            If there is only one valid value for a cell, it is set in __board.
+        """
+        self.__update_values()
         for i in range(9):
             for j in range(9):
-                if self.__board[i, j] != 0:
-                    __assert_valid_value(self.__board[i, j])
-                    self.__values[i, j, :] = False
-                    self.__values[i, j, self.__board[i, j] - 1] = True
+                if self.__board[i, j] == 0:
+                    # There is only one valid value for the cell
+                    print(np.sum(self.__values[i, j, :]), end=",   ")
+                    if np.sum(self.__values[i, j, :]) == 1:
+                        valid_values = np.where(self.__values[i, j, :])[0]
+                        print(valid_values)
+                        self.__board[i, j] = valid_values[0] + 1
--- tests/test_board.py
+++ tests/test_board.py
@@ -1,4 +1,25 @@
 from sudoku.board import Board
+from sudoku.board import _check_valid_value
+from sudoku.board import _check_no_duplicates
+
+
+def test__check_valid_value():
+    """ Test the _check_valid_value function """
+    assert _check_valid_value(0), "Error in _check_valid_value."
+    assert _check_valid_value(1), "Error in _check_valid_value."
+    assert _check_valid_value(9), "Error in _check_valid_value."
+    assert not _check_valid_value(10), "Error in _check_valid_value."
+    assert not _check_valid_value(-1), "Error in _check_valid_value."
+    
+    
+def test__check_duplicates():
+    """ Test the _check_duplicates function """
+    assert _check_no_duplicates([0, 1, 2, 3, 4, 5, 6, 7, 8, 9]), \
+        "Error in _check_duplicates."
+    assert _check_no_duplicates([0, 1, 2, 3, 4, 5, 6, 7, 8, 0]), \
+        "Error in _check_duplicates."
+    assert not _check_no_duplicates([0, 1, 2, 1, 0]), \
+        "Error in _check_duplicates."
 
 
 def test_board_constructor():
@@ -7,4 +28,14 @@ def test_board_constructor():
 
     for i in range(9):
         for j in range(9):
-            assert b(i, j) == 0, "Error in the constructor"
+            assert b[i, j] == 0, \
+                "Error in the __getitem__ method or in the "
+
+    b[0, 0] = 1
+    assert b[0, 0] == 1, "Error in the __setitem__ method"
+
+    b[0, 1] = 1
+    assert not b.isValid()
+    
+    b[0, 1] = 0
+    
